ec7d2a87c766d96d8b0eaefc81c6964698169349
fabio bueno <fabiobueno@outlook.com>
2024-09-15 19:14:43 -0300

    Ajuste query endpoint GET pedidos

--- app/models/pedidos.model.js
+++ app/models/pedidos.model.js
@@ -8,7 +8,7 @@ const Pedido = function(pedido) {
 };
 
 Pedido.getAll = result => {
-  sql.query("SELECT p.id, p.id_pedido_impresso, c.nome, p.data_pedido_realizado, p.data_entrega_realizada_1, p.condicoes_de_pagto, COUNT(i.id) AS itens, SUM(i.quantidade) AS quantidade_total_itens, SUM(i.valor) * i.quantidade AS valor_soma_itens, e.nome AS status_entrega FROM `pedidos` AS p LEFT JOIN pedidos_itens AS i ON p.id = i.id_pedido LEFT JOIN clientes AS c ON p.id_cliente = c.id LEFT JOIN entrega_status_nomes AS e ON p.status_entrega = e.id", (err, res) => {
+  sql.query("SELECT p.id, p.id_pedido_impresso, c.nome, p.data_pedido_realizado, p.data_entrega_realizada_1, p.condicoes_de_pagto, COUNT(i.id) AS itens, SUM(i.quantidade) AS quantidade_total_itens, e.nome AS status_entrega FROM `pedidos` AS p LEFT JOIN pedidos_itens AS i ON p.id = i.id_pedido LEFT JOIN clientes AS c ON p.id_cliente = c.id LEFT JOIN entrega_status_nomes AS e ON p.status_entrega = e.id", (err, res) => {
     if (err) {
       console.log("error: ", err);
       result(null, err);
